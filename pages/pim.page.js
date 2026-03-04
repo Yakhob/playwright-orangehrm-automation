96815afc9d7550bd122f1a6496b4d638703bd899
Yakhob <jacobkurian2004@gmail.com>
2026-03-04 20:58:28 +0530

    Refactor PimPage class and reorganize methods

--- pages/pim.page.js
+++ pages/pim.page.js
@@ -1,31 +1,24 @@
-\import { expect } from '@playwright/test';
+import { expect } from '@playwright/test';
 
 class PimPage {
     constructor(page) {
         this.page = page;
         
         this.pimMenu = page.locator('a[href*="pim/viewPimModule"]');
-        this.employeeListMenu = page.getByRole('link', { name: 'Employee List' });
-        this.addEmployeeMenu = page.getByRole('link', { name: 'Add Employee' });
-
-   
         this.firstNameInput = page.locator('input[name="firstName"]');
         this.lastNameInput = page.locator('input[name="lastName"]');
-        this.middleNameInput = page.locator('input[name="middleName"]');
-
-
         this.saveButton = page.getByRole('button', { name: 'Save' }).first();
+
+       
+        this.employeeTable = page.locator('.oxd-table-body'); 
+        this.noRecordsMessage = page.locator('.orangehrm-horizontal-padding > .oxd-text');
+        this.successToast = page.locator('.oxd-toast-content-text').last();
+       
+
+        this.employeeNameSearch = page.getByPlaceholder('Type for hints...').first();
         this.searchButton = page.getByRole('button', { name: 'Search' });
         this.confirmDeleteButton = page.getByRole('button', { name: 'Yes, Delete' });
-
-      
-        this.employeeNameSearch = page.getByPlaceholder('Type for hints...').first();
-        this.employeeTable = page.locator('.oxd-table-body'); 
-        
- 
-        this.successToast = page.locator('.oxd-toast-content-text').last();
     }
-
     async navigateToPIM() {
         await this.pimMenu.click();
         await this.employeeNameSearch.waitFor({ state: 'visible', timeout: 15000 });
@@ -40,6 +33,8 @@ class PimPage {
         await this.firstNameInput.fill(firstName);
         await this.lastNameInput.fill(lastName);
         await this.saveButton.click();
+        
+        
         await expect(this.successToast).toHaveText(/Successfully Saved/, { timeout: 10000 });
         await this.page.getByRole('heading', { name: 'Personal Details' }).waitFor();
     }
@@ -47,6 +42,8 @@ class PimPage {
     async searchEmployee(name) {
         await this.employeeListMenu.click();
         await this.employeeNameSearch.fill(name);
+        
+        
         const suggestion = this.page.locator('.oxd-autocomplete-dropdown .oxd-autocomplete-option');
         await suggestion.first().waitFor({ state: 'visible' });
         await suggestion.first().click();
@@ -56,10 +53,12 @@ class PimPage {
     }
 
     async editEmployee(middleName) {
+       
         await this.page.locator('.oxd-table-cell-actions .bi-pencil-fill').first().click();
         
         await this.page.getByRole('heading', { name: 'Personal Details' }).waitFor();
         await this.page.waitForLoadState('networkidle');
+
         await this.middleNameInput.click({ clickCount: 3 });
         await this.page.keyboard.press('Backspace');
         await this.middleNameInput.fill(middleName);
